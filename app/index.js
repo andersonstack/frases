d9de69d0f9a936f218250e7e39a16bd028582d99
andersonstack <andersong.pereiracruz@gmail.com>
2025-02-09 10:57:34 -0300

    docs: :memo: adicionando comentários

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 (async () => {
+  // Services
   const fraseApi = new FraseApi();
   const resultado = await fraseApi.obterFrase();
+
+  // Elementos do service
   const frase = resultado.frase;
   const id = resultado.id;
+
   // Controller
   const fraseController = new FraseController(frase, id);
 })();
