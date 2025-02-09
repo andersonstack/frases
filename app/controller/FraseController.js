a04fc91cdea018782f3abc1450baa045de54763f
andersonstack <andersong.pereiracruz@gmail.com>
2025-02-09 11:27:26 -0300

    feat: :sparkles: adicionando evento ao botão

--- app/controller/FraseController.js
+++ app/controller/FraseController.js
@@ -1,13 +1,37 @@
 class FraseController {
   constructor(frase, id) {
-    // document
-    let $ = document.querySelector.bind(document);
-
+    // Informações
     this._frase = frase;
     this._id = id;
 
+    // Document
+    let $ = document.querySelector.bind(document);
+
+    // Elementos do DOM
     this._fraseHtml = $("#frase");
+    this._fraseLista = $("#lista");
+    this._btnSalvar = $("#salvar");
+
+    // Instância da View - frase principal
     this._fraseView = new FraseView(this._fraseHtml, this._frase, this._id);
     this._fraseView.update(this._frase);
+
+    // Instância da View - lista de frases
+    this._listaView = new ListaView(this._fraseLista);
+
+    // Garante que o botão está no DOM antes de adicionar o evento
+    this._adicionarEvento();
+  }
+
+  _adicionarEvento() {
+    if (this._btnSalvar) {
+      document.querySelector("#frase").addEventListener("click", (event) => {
+        if (event.target.id === "salvar") {
+          console.log("Clicou no botão!");
+        }
+      });
+    } else {
+      console.error("Botão 'Salvar' não encontrado no DOM!");
+    }
   }
 }
