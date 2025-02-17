f214c726fbabfd3fc9ec61b1c69dc7b2e54f2204
andersonstack <andersong.pereiracruz@gmail.com>
2025-02-17 13:59:07 -0300

    refactor: :recycle: movendo funcionalidades

--- app/index.js
+++ app/index.js
@@ -10,3 +10,19 @@
   // Controller
   const fraseController = new FraseController(frase, id);
 })();
+
+let $ = document.querySelector.bind(document);
+
+// let btn = $("#reload").addEventListener("click", async () => {
+//   await ReloadController.recarregar();
+// });
+
+$("#about").addEventListener("click", function () {
+  const aboutMobile = document.querySelector(".about-mobile");
+  aboutMobile.classList.toggle("expanded");
+});
+
+$("#contacts").addEventListener("click", function () {
+  const aboutMobile = document.querySelector(".contact-mobile");
+  aboutMobile.classList.toggle("expanded");
+});
